c73adb8c0d9d872a310f9116990949ed76d44dcb
Jake Stewart <jakey@snakey.local>
2023-06-10 12:56:06 +0800

    remove spaces from key names

--- src/key.cpp
+++ src/key.cpp
@@ -10,18 +10,18 @@ std::string getKeyName(Key key) {
         case K_CTRL_A ... (K_TAB - 1):
         case (K_TAB + 1) ... (K_ENTER - 1):
         case (K_ENTER + 1) ... K_CTRL_Z:
-            return std::string("CTRL '") + (char)(K_A + key - 1) + "'";
+            return std::string("CTRL-") + (char)(K_A + key - 1) + "'";
 
         case K_ESCAPE:
             return "ESCAPE";
         case K_FS:
             return "FS";
         case K_CTRL_CLOSE_BRACKET:
-            return "CTRL ']'";
+            return "CTRL-]";
         case K_CTRL_BACKSLASH:
-            return "CTRL '\\'";
+            return "CTRL-\\";
         case K_CTRL_SLASH:
-            return "CTRL '/'";
+            return "CTRL-/";
 
         case K_SPACE ... K_TILDE:
             return std::string("'") + (char)key + "'";
@@ -30,17 +30,17 @@ std::string getKeyName(Key key) {
             return "BACKSPACE";
 
         case K_ALT_BACKSPACE:
-            return "ALT BACKSPACE";
+            return "ALT-BACKSPACE";
 
         case K_DELETE:
             return "DELETE";
 
         case K_ALT_TAB:
-            return "ALT TAB";
+            return "ALT-TAB";
         case K_ALT_ENTER:
-            return "ALT ENTER";
+            return "ALT-ENTER";
         case K_ALT_SPACE ... K_ALT_TILDE:
-            return std::string("ALT '")
+            return std::string("ALT-'")
                 + (char)(K_SPACE + key - K_ALT_SPACE) + "'";
 
         case K_UP:
@@ -62,30 +62,30 @@ std::string getKeyName(Key key) {
             return "SHIFT LEFT";
 
         case K_CTRL_UP:
-            return "CTRL UP";
+            return "CTRL-UP";
         case K_CTRL_DOWN:
-            return "CTRL DOWN";
+            return "CTRL-DOWN";
         case K_CTRL_RIGHT:
-            return "CTRL RIGHT";
+            return "CTRL-RIGHT";
         case K_CTRL_LEFT:
-            return "CTRL LEFT";
+            return "CTRL-LEFT";
 
         case K_CTRL_TAB:
-            return "CTRL TAB";
+            return "CTRL-TAB";
         case K_CTRL_SHIFT_TAB:
-            return "CTRL SHIFT TAB";
+            return "CTRL-SHIFT-TAB";
 
         case K_ALT_UP:
-            return "ALT UP";
+            return "ALT-UP";
         case K_ALT_DOWN:
-            return "ALT DOWN";
+            return "ALT-DOWN";
         case K_ALT_RIGHT:
-            return "ALT RIGHT";
+            return "ALT-RIGHT";
         case K_ALT_LEFT:
-            return "ALT LEFT";
+            return "ALT-LEFT";
 
         case K_ALT_SHIFT_TAB:
-            return "ALT SHIFT TAB";
+            return "ALT-SHIFT-TAB";
 
         case K_F1 ... K_F12:
             return std::string("F") + std::to_string(key - K_F1 + 1);
